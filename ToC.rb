a3af90348eb6fddd3c300b858a4115f824ec1fba
David T <david@digitallifecyclemarketing.com>
2024-06-13 16:16:32 -0700

    Add files via upload
    Initial commit

---
+++ ToC.rb
@@ -0,0 +1,5 @@
+line_width = 100
+puts ("Table of Contents".center (line_width))
+puts ("Chapter 1: Numbers".ljust(line_width/2) + "page 1".rjust(line_width/2))
+puts ("Chapter 2: Letters".ljust(line_width/2) + "page 5".rjust(line_width/2))
+puts ("Chapter 3: Variables".ljust(line_width/2) + "page 9".rjust(line_width/2))
